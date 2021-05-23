c023f5334a6e7b8beb3dc4bb82cd1b07b5f42ebe
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2021-05-23 19:26:25 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Holo-HUD Speech API
+
+## Description:
+See Holo-HUD README.md for project details.
+
+## Front_End Link: https://github.com/ak89mkii/holo-hud
---
+++ main.py
@@ -0,0 +1,66 @@
+import speech_recognition as sr;
+import webbrowser;
+import random;
+import os;
+import playsound;
+from gtts import gTTS;
+from flask import Flask, redirect, url_for, render_template, request;
+from time import ctime;
+
+app = Flask(__name__)
+
+r = sr.Recognizer()
+
+@app.route("/")
+def home():
+    sayings_list = ['hello', 'whats up', 'good day', 'do you have a question']
+    mom_9000_speak(random.choice(sayings_list))
+    voice_data = record_audio()
+    respond(voice_data)
+    return render_template('index.html')
+
+def record_audio(ask=False):
+    with sr.Microphone() as source:
+        if ask:
+            mom_9000_speak(ask)
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            mom_9000_speak('im sorry my responses are limited. you must ask the right questions')
+        except sr.RequestError:
+            mom_9000_speak('Sorry, speech service down.')
+        return voice_data
+
+def mom_9000_speak(audio_string):
+    tts = gTTS(text=audio_string, lang='en')
+    r = random.randint(1, 1000000)
+    audio_file = 'audio-' + str(r) + '.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        mom_9000_speak('My name is holo mom 9000.')
+    if 'what time is it' in voice_data:
+        mom_9000_speak(ctime())
+    if 'who is the best' in voice_data:
+        mom_9000_speak('Rocket is the best.')
+    if 'say something' in voice_data:
+        sayings_list = ['what a cute little gnome', 'jesus mary and joseph', 'shut the explitive deleted up', 'matka boska', 'brothers and sister brothers and sister are they nice', 'eat all the junk food you want']
+        mom_9000_speak(random.choice(sayings_list))
+    if 'cook' in voice_data:
+        mom_9000_speak('jesus mary and joseph.')
+
+
+# @app.route('/activate', methods=['POST'])
+# def activate(mom_9000_speak):
+#     mom_9000_speak('How may I be of service?')
+#     voice_data = record_audio()
+#     respond(voice_data)
+
+if __name__=="__main__":
+    app.run(port=5000, debug=True, threaded=True)
